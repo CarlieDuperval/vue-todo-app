1e8fb2968700d7c31960e1bdda5b3ef09dad75f0
Carlie Duperval <ritchellcarlie@gmail.com>
2022-06-27 15:55:26 -0400

    Added script to setup

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,38 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import: ref to handle the state
+// onMounted: use like a local storage when we first render tha page
+// computed :to  have the data in order , will order the list by created date
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref([]) // creat an empty array to store todo items
+const name = ref('') // user name
+const input_content = ref('') // is the field for the content of todo
+const input_category = ref (null) // is the feild for the category of todo 
+
+const todos_ascending = computed(
+  () => 
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  }) // create at date
+  )
+
+  const addTodo = () => {
+    if(input_content.value.trim() === "" || input_category === null){
+      return
+    }
+  }
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<main class="app">
+  <section class="greeting">
+    <h2 class="title">
+      What's up , <input type="text" placeholder="Name here" v-model="name" />
+    </h2>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  </section>
 
-  <main>
-    <TheWelcome />
-  </main>
+</main>
 </template>
 
-<style>
-@import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
-</style>
